d276d56ac82822aee7b9d3cfd039b13465121d04
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 13:03:02 -0800

    third version using Counter

--- wordcount.py
+++ wordcount.py
@@ -19,26 +19,54 @@
 
 # my_file.close()
 
+
+#version2
+# import sys
+
+
+# def find_wordcount(file_name):
+    
+#     my_file = open(file_name)
+
+#     dict_word_count = {}
+
+#     for line in my_file:
+#         word_list = line.strip().split(" ")
+#         for word in word_list:
+#             word = word.lower().strip('"').strip(",").strip(".").strip("?")\
+#             .strip(":").strip(";").strip("!")
+#             dict_word_count[word] = dict_word_count.get(word, 0) + 1
+
+
+#     for word, count in dict_word_count.items():
+#         print word, count
+
+
+#     my_file.close()
+
+# find_wordcount(sys.argv[1])
+
+
 import sys
+from collections import Counter
+
+counter = Counter()  # makes empty counter dictionary
 
 
 def find_wordcount(file_name):
-    
     my_file = open(file_name)
-
-    dict_word_count = {}
 
     for line in my_file:
         word_list = line.strip().split(" ")
-        for word in word_list:
-            word = word.lower().strip('"').strip(",").strip(".").strip("?")\
-            .strip(":").strip(";").strip("!")
-            dict_word_count[word] = dict_word_count.get(word, 0) + 1
+        counter.update(word_list)
+    # print counter
+        # for word in word_list:
+        #     word = word.lower().strip('"').strip(",").strip(".").strip("?")\
+        #     .strip(":").strip(";").strip("!")
+        #     dict_word_count[word] = dict_word_count.get(word, 0) + 1
 
-
-    for word, count in dict_word_count.items():
+    for word, count in counter.items():
         print word, count
-
 
     my_file.close()
 
